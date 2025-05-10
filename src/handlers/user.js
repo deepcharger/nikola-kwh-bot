1c0a8e07af9015b456731787f6b38553dba44522
deepcharger <github-deep.pp322@slmails.com>
2025-05-10 11:01:51 +0200

    Update user.js

--- src/handlers/user.js
+++ src/handlers/user.js
@@ -19,37 +19,38 @@ const showHelp = async (ctx) => {
     
     // Comandi per tutti gli utenti
     message += '👤 Comandi utente\n';
-    message += '/start - Avvia il bot / Registrazione\n';
-    message += '/help - Mostra questo messaggio di aiuto\n';
-    message += '/saldo - Visualizza il tuo saldo kWh attuale\n';
-    message += '/cronologia - Visualizza la cronologia delle tue transazioni\n';
-    message += '/registra_utilizzo - Registra un nuovo utilizzo di kWh\n';
-    message += '/profilo - Visualizza il tuo profilo\n\n';
+    message += '`/start` - Avvia il bot / Registrazione\n';
+    message += '`/help` - Mostra questo messaggio di aiuto\n';
+    message += '`/saldo` - Visualizza il tuo saldo kWh attuale\n';
+    message += '`/cronologia` - Visualizza la cronologia delle tue transazioni\n';
+    message += '`/registra_utilizzo` - Registra un nuovo utilizzo di kWh\n';
+    message += '`/profilo` - Visualizza il tuo profilo\n\n';
     
     // Comandi per amministratori
     if (isAdmin) {
       message += '👑 Comandi amministratore\n';
-      message += '/admin_utenti - Visualizza la lista degli utenti paginata\n';
-      message += '/admin_trova_tessera [numero] - Cerca utente per tessera\n';
-      message += '/admin_trova_utente [nome o @username] - Cerca utente\n';
-      message += '/admin_dettaglio [ID_Telegram] - Mostra dettagli utente\n';
-      message += '/admin_approva [ID_Telegram] - Approva un utente\n';
-      message += '/admin_blocca [ID_Telegram] - Blocca un utente\n';
-      message += '/admin_sblocca [ID_Telegram] - Sblocca un utente\n';
-      message += '/admin_disabilita [ID_Telegram] - Disabilita un utente\n';
-      message += '/admin_elimina [ID_Telegram] - Elimina definitivamente un utente\n';
-      message += '/admin_conferma_eliminazione [ID_Telegram] - Conferma eliminazione\n';
-      message += '/admin_ricarica [ID_Telegram/@username/tessera:XX] - Ricarica un utente\n';
-      message += '/admin_crea_invito - Crea un nuovo codice di invito\n';
-      message += '/admin_inviti - Visualizza la lista dei codici di invito\n';
-      message += '/admin_esporta_utenti - Esporta lista utenti in CSV\n';
-      message += '/admin_stats - Visualizza le statistiche del bot\n';
-      message += '/admin_make_admin [ID_Telegram] - Promuovi un utente ad amministratore\n';
-      message += '/admin_aggiorna_comandi - Aggiorna i comandi bot\n';
-      message += '/admin_saldi_bassi - Trova utenti con saldo basso\n';
+      message += '`/admin_utenti` - Visualizza la lista degli utenti paginata\n';
+      message += '`/admin_trova_tessera [numero]` - Cerca utente per tessera\n';
+      message += '`/admin_trova_utente [nome o @username]` - Cerca utente\n';
+      message += '`/admin_dettaglio [ID_Telegram]` - Mostra dettagli utente\n';
+      message += '`/admin_approva [ID_Telegram]` - Approva un utente\n';
+      message += '`/admin_blocca [ID_Telegram]` - Blocca un utente\n';
+      message += '`/admin_sblocca [ID_Telegram]` - Sblocca un utente\n';
+      message += '`/admin_disabilita [ID_Telegram]` - Disabilita un utente\n';
+      message += '`/admin_elimina [ID_Telegram]` - Elimina definitivamente un utente\n';
+      message += '`/admin_conferma_eliminazione [ID_Telegram]` - Conferma eliminazione\n';
+      message += '`/admin_ricarica [ID_Telegram/@username/tessera:XX]` - Ricarica un utente\n';
+      message += '`/admin_crea_invito` - Crea un nuovo codice di invito\n';
+      message += '`/admin_inviti` - Visualizza la lista dei codici di invito\n';
+      message += '`/admin_esporta_utenti` - Esporta lista utenti in CSV\n';
+      message += '`/admin_stats` - Visualizza le statistiche del bot\n';
+      message += '`/admin_make_admin [ID_Telegram]` - Promuovi un utente ad amministratore\n';
+      message += '`/admin_aggiorna_comandi` - Aggiorna i comandi bot\n';
+      message += '`/admin_saldi_bassi` - Trova utenti con saldo basso\n';
+      message += '`/admin_ricariche` - Visualizza le ultime ricariche\n';
     }
     
-    return ctx.reply(message);
+    return ctx.reply(message, { parse_mode: 'Markdown' });
   } catch (error) {
     console.error('Errore durante la visualizzazione dell\'aiuto:', error);
     return ctx.reply('Si è verificato un errore. Per favore, riprova più tardi.');
